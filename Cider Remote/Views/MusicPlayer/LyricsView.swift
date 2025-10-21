7e9f39f52c66fed1a5469ed0954d2ac5292afbf8
lumaa-dev <lumaa@lumaa.fr>
2025-10-21 12:00:25 +0200

    optimization

--- Cider Remote/Views/MusicPlayer/LyricsView.swift
+++ Cider Remote/Views/MusicPlayer/LyricsView.swift
@@ -49,6 +49,7 @@ struct LyricsView: View {
                                 if userDevice.horizontalOrientation == .portrait || userDevice.isPad {
                                     LyricsScrollView(
                                         lyrics: lyrics,
+                                        track: currentTrack,
                                         activeLine: $activeLine,
                                         currentTime: $currentTime,
                                         viewportHeight: geometry.size.height,
@@ -249,6 +250,7 @@ struct LyricsScrollView: View {
     @EnvironmentObject private var device: Device
 
     let lyrics: [LyricLine]
+    let track: Track?
     @Binding var activeLine: LyricLine?
 
     @Binding var currentTime: Double
@@ -260,7 +262,6 @@ struct LyricsScrollView: View {
 
     @State private var isDragging: Bool = false
 
-    @State private var sharingTrack: Track? = nil
     @State private var sharingLyric: LyricLine? = nil
 
     var body: some View {
@@ -301,7 +302,7 @@ struct LyricsScrollView: View {
                                 } label: {
                                     Label("Share lyric", systemImage: "square.and.arrow.up")
                                 }
-                                .disabled(self.sharingTrack == nil)
+                                .disabled(self.track == nil)
                             }
                         }
                         Spacer(minLength: viewportHeight - 180) // Remaining space below lyrics
@@ -315,7 +316,7 @@ struct LyricsScrollView: View {
                         print(self.sharingLyric ?? "just checking yk?")
                     }
                 } content: { lyric in
-                    if let track = self.sharingTrack {
+                    if let track = self.track {
                         LyricShare(track: track, lyric: lyric)
                     } else {
                         ProgressView()
@@ -323,9 +324,6 @@ struct LyricsScrollView: View {
                                 self.sharingLyric = nil
                             }
                     }
-                }
-                .task {
-                    self.sharingTrack = await self.getCurrentTrack()
                 }
                 .scrollClipDisabled()
                 .onChange(of: activeLine) { _, newActiveLine in
@@ -340,68 +338,6 @@ struct LyricsScrollView: View {
             }
         }
         .frame(height: viewportHeight)
-    }
-
-    func getCurrentTrack() async -> Track? {
-        print("Fetching current track")
-        do {
-            let data = try await device.sendRequest(endpoint: "playback/now-playing", method: "GET")
-
-            if let jsonDict = data as? [String: Any], let info = jsonDict["info"] as? [String: Any] {
-                return self.getTrack(using: info)
-            } else {
-                throw NetworkError.decodingError
-            }
-        } catch {
-            print(error)
-        }
-        
-        return nil
-    }
-
-    private func getTrack(using info: [String: Any]) -> Track {
-        // Extract ID from playParams
-        var id: String?
-        var amId: String?
-
-        if let playParams = info["playParams"] as? [String: Any] {
-            id = playParams["id"] as? String
-            amId = playParams["catalogId"] as? String
-        }
-
-        let title = info["name"] as? String ?? ""
-        let artist = info["artistName"] as? String ?? ""
-        let album = info["albumName"] as? String ?? ""
-        let duration = info["durationInMillis"] as? Double ?? 0
-
-        if let artwork = info["artwork"] as? [String: Any],
-           var artworkUrl = artwork["url"] as? String {
-            // Replace placeholders in artwork URL
-            artworkUrl = artworkUrl.replacingOccurrences(of: "{w}", with: "1024")
-            artworkUrl = artworkUrl.replacingOccurrences(of: "{h}", with: "1024")
-
-            let data: Data? = nil
-
-            return Track(id: id ?? "",
-                         catalogId: amId ?? "",
-                         title: title,
-                         artist: artist,
-                         album: album,
-                         artwork: artworkUrl,
-                         duration: duration / 1000,
-                         artworkData: data ?? Data()
-            )
-        } else {
-            return Track(id: id ?? "",
-                         catalogId: amId ?? "",
-                         title: title,
-                         artist: artist,
-                         album: album,
-                         artwork: "",
-                         duration: duration / 1000,
-                         artworkData: Data()
-            )
-        }
     }
 }
 
